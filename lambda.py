1446942898759369664d7fea29543c00aebd76ef
AmitRanjan235 <99864721+AmitRanjan235@users.noreply.github.com>
2023-09-10 11:55:26 +0530

    lambda function added

--- lambda.py
+++ lambda.py
@@ -1,15 +1,11 @@
-"""
-serializeImageData: a lambda function for pull an image from S3 and returning serializing data
-"""
 import json
 import boto3
 import base64
 
-
 #For error handling
 import botocore
 
-s3 = boto3.client('s3')
+s3 = boto3.resource('s3')
 
 def lambda_handler(event, context):
     """A function to serialize target data from S3"""
@@ -20,16 +16,12 @@ def lambda_handler(event, context):
     
     # Download the data from s3 to /tmp/image.png
     try:
-    s3.Bucket(bucket).download_file(key, '/tmp/image.png')
+        s3.Bucket(bucket).download_file(key, '/tmp/image.png')
     except botocore.exceptions.ClientError as e:
-    error_code = e.response['Error']['Code']
-    error_message = e.response['Error']['Message']
-    
-    if error_code == "404":
-        print("The object does not exist.")
-    else:
-        print(f"An S3 error occurred: {error_code} - {error_message}")
-
+        if e.response['Error']['Code'] == "404":
+            print("The object does not exist.")
+        else:
+            raise
 
     # We read the data from a file
     with open("/tmp/image.png", "rb") as f:
@@ -41,41 +33,58 @@ def lambda_handler(event, context):
         'statusCode': 200,
         'body': {
             "image_data": image_data,
-            "s3_bucket": bucket,
-            "s3_key": key,
-            "inferences": []
+            
         }
     }
 
 ## classification
 
 import json
-import sagemaker
 import base64
-from sagemaker.serializers import IdentitySerializer
+import boto3
 
 # Fill this in with the name of your deployed model
-ENDPOINT = "your_endpoint_name"  # Replace with your SageMaker endpoint name
+ENDPOINT = "image-classification-2023-09-09-08-03-38-433"  # Replace with your SageMaker endpoint name
+runtime = boto3.client('runtime.sagemaker')
 
 def lambda_handler(event, context):
-    # Decode the image data
-    image = base64.b64decode(event['image_data'])  
+    try:
+        # Log the incoming event for debugging purposes
+        print("Received event:", json.dumps(event))
+        print("Event keys:", event.keys())
 
-    # Instantiate a Predictor
-    predictor = sagemaker.predictor.Predictor(
-        endpoint_name=ENDPOINT,
-        serializer=IdentitySerializer("image/png"),  # Use the appropriate serializer
-    )
+        # Check if "image_data" key exists in the event
+        if "image_data" not in event:
+            raise KeyError("Missing 'image_data' key in event")
 
-    # Make a prediction
-    inferences = predictor.predict(image_data)
+        # Decode the image data
+        image = base64.b64decode(event["image_data"])
 
-    # We return the data back to the Step Function
-    event["inferences"] = inferences.decode('utf-8')
-    return {
-        'statusCode': 200,
-        'body': json.dumps(event)
-    }
+        # Log that image decoding was successful
+        print("Image decoding successful")
+
+        # Instantiate a Predictor
+        response = runtime.invoke_endpoint(EndpointName=ENDPOINT, ContentType='application/x-image', Body=image)
+        inferences = response['Body'].read().decode('utf-8')
+        event["inferences"] = [float(x) for x in inferences[1:-1].split(',')]
+
+        # Log the inferences
+        print("Inferences:", event["inferences"])
+
+        # We return the data back to the Step Function
+        return {
+            'statusCode': 200,
+            'body': {
+                "image_data": event['image_data'],
+                "s3_bucket": event.get('s3_bucket'),  # Use .get() to avoid KeyError if key is missing
+                "s3_key": event.get('s3_key'),        # Use .get() to avoid KeyError if key is missing
+                "inferences": event['inferences'],
+            }
+        }
+    except Exception as e:
+        # Log any exceptions that occur
+        print("Error:", str(e))
+        raise e  # Re-raise the exception for proper error handling
 
 
 ## low confidence Filteration
@@ -89,7 +98,7 @@ THRESHOLD = .93
 def lambda_handler(event, context):
     
     # Grab the inferences from the event
-    inferences =event(['inferences']) ## TODO: fill in
+    inferences =event['inferences'] ## TODO: fill in
     
     # Check if any values in our inferences are above THRESHOLD
     meets_threshold =any(x>= THRESHOLD for x in inferences) ## TODO: fill in
